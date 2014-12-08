91630c191e81f06ab59f6d00b034ca1ec71d14b8
Karen Kim <karenskim19@gmail.com>
2014-12-07 16:32:14 -0800

    Basic page structure and styling. Must start working on functionality ASAP.

--- js/tttsController.js
+++ js/tttsController.js
@@ -5,3 +5,20 @@ angular
 function tttsController() {
   
 }
+
+
+//  PLAYER CONSTRUCTOR FUNCTION
+
+function player() {
+  blahblahblah
+  things shalala 
+  if ((A1+A2+A3) || (B1+B2+b3) || (C1+C2+C3) || 
+     (A1+B1+C1)  || (A2+B2+C2) || (A3+B3+C3) ||
+     (A1+B2+C3)  || (A3+B2+C1)) {
+    return "winner"
+  }
+
+var player1 = new player()
+var player2 = new player()
+
+}
