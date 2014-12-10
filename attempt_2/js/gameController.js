34e2d85805eb309ea6d9197390a858a5e9bc50fd
Karen Kim <karenskim19@gmail.com>
2014-12-10 15:39:06 -0800

    fresh start. ng-repeat through Controller array of gamepieces in html div

--- attempt_2/js/gameController.js
+++ attempt_2/js/gameController.js
@@ -2,47 +2,37 @@ angular
   .module('gameApp')
   .controller('GameController', GameController);
 
-function GameController() {
+function GameController(){
 
   this.userChoice = userChoice; 
   this.square     = document.getElementsByClassName('square');
   this.clickCount = clickCount; 
-  this.gameBoard  = [ 
-    {square: "a1", playerClicked: ""},
-    {square: "b1", playerClicked: ""},
-    {square: "c1", playerClicked: ""},
+  this.gamePieces = [ 
+    {"square": "a1", "playerClicked": false},
+    {"square": "b1", "playerClicked": false},
+    {"square": "c1", "playerClicked": false},
 
-    {square: "a2", playerClicked: ""},
-    {square: "b2", playerClicked: ""},
-    {square: "c2", playerClicked: ""},
+    {"square": "a2", "playerClicked": false},
+    {"square": "b2", "playerClicked": false},
+    {"square": "c2", "playerClicked": false},
 
-    {square: "a3", playerClicked: ""},
-    {square: "b3", playerClicked: ""},
-    {square: "c3", playerClicked: ""},
+    {"square": "a3", "playerClicked": false},
+    {"square": "b3", "playerClicked": false},
+    {"square": "c3", "playerClicked": false},
   ];
 }
 
-// function checkWins() {
-
-// }
-
-// function players() {
-//   this.players = [];
-// }
-
-
-function userChoice(square) {
-  for (var i = 0; i < this.square.length; i++) {
+function userChoice(square){
+  for (var i = 0; i < this.square.length; i++){
     this.square[i].innerText = "X";
   }
 }
 
-
-
 // THIS DETERMINES WHICH PLAYER'S TURN IT IS (CURRENTLY ONLY WORKS IN CONSOLE)
 var i = 0;
-function clickCount() {
+function clickCount(){
   i += 1;
+  // this.square.value = i;
   document.getElementById("square1").value = i;
   if (i % 2 === 0) {
    console.log("Player 2 Turn");
@@ -50,4 +40,5 @@ function clickCount() {
   else {
    console.log("Player 1 Turn");
   }
+
 }
