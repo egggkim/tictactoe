c6e4fdb0fd098217ba7dcdd7a99517d93f8d6334
Karen Kim <karenskim19@gmail.com>
2014-12-09 20:42:45 -0800

    trying out ng-repeat in index.html and array gameBoard in controller.

--- attempt_2/js/gameController.js
+++ attempt_2/js/gameController.js
@@ -7,6 +7,19 @@ function GameController() {
   this.userChoice = userChoice; 
   this.square     = document.getElementsByClassName('square');
   this.clickCount = clickCount; 
+  this.gameBoard  = [ 
+    {square: "a1", playerClicked: ""},
+    {square: "b1", playerClicked: ""},
+    {square: "c1", playerClicked: ""},
+
+    {square: "a2", playerClicked: ""},
+    {square: "b2", playerClicked: ""},
+    {square: "c2", playerClicked: ""},
+
+    {square: "a3", playerClicked: ""},
+    {square: "b3", playerClicked: ""},
+    {square: "c3", playerClicked: ""},
+  ];
 }
 
 // function checkWins() {
